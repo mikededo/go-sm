af8d40e5a871223a7847d2830b8ad99210394745
Miquel de Domingo <miquelddg@gmail.com>
2023-02-11 08:22:21 +0100

    feat(app): migrate `FindUserByUsername` tests

--- server/application/user/find_user_by_username_test.go
+++ server/application/user/find_user_by_username_test.go
@@ -7,13 +7,13 @@ import (
 	"github.com/mddg/go-sm/server/application"
 	"github.com/mddg/go-sm/server/application/user"
 	userEntity "github.com/mddg/go-sm/server/domain/user"
+	"github.com/stretchr/testify/assert"
 )
 
 func validateFindByUserUsernameCalls(t *testing.T, s *UserRepositorySpy) {
 	s.CalledOnce(t)
-	if s.Calls[0] != "mikededo" {
-		t.Errorf("repository expected to be called with %s received %s\n", "mikededo", s.Calls[0])
-	}
+	assert.Equal(t, s.Calls[0], "mikededo",
+		"repository expected to be called with %s received %s\n", "mikededo", s.Calls[0])
 }
 
 func TestFindUserByUsernameService_Run(t *testing.T) {
@@ -27,12 +27,8 @@ func TestFindUserByUsernameService_Run(t *testing.T) {
 		res, err := service.Run("mikededo")
 
 		validateFindByUserUsernameCalls(t, spy)
-		if err != nil {
-			t.Errorf("error was not expected, got %v\n", err)
-		}
-		if res.ID != 1 || res.Username != "mikededo" {
-			t.Errorf("expected %v user got %v", u, res)
-		}
+		assert.Nil(t, err, "error was not expected, got %v\n", err)
+		assert.False(t, res.ID != 1 || res.Username != "mikededo", "expected %v user got %v", u, res)
 	})
 
 	t.Run("user not found", func(t *testing.T) {
@@ -44,11 +40,7 @@ func TestFindUserByUsernameService_Run(t *testing.T) {
 		res, err := service.Run("mikededo")
 
 		validateFindByUserUsernameCalls(t, spy)
-		if err == nil {
-			t.Errorf("expecting error got %v\n", err)
-		}
-		if res != nil {
-			t.Errorf("user was not expected, got %v", res)
-		}
+		assert.NotNil(t, err, "expecting error got %v\n", err)
+		assert.Nil(t, res, "user was not expected, got %v", res)
 	})
 }
